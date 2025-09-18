d3571cdaa40610620da0391598668cc1ce3ed523
Viktor Tanyi <vtanyi@hiya.com>
2025-09-18 12:10:32 +0200

    Add withOverrides, etc. to TableLike, etc.

--- core/src/main/scala/com/hiya/alternator/Table.scala
+++ core/src/main/scala/com/hiya/alternator/Table.scala
@@ -10,10 +10,52 @@ sealed trait TableLike[C <: DynamoDBClient, V, PK] {
   val schema: TableSchema.Aux[V, PK]
   val overrides: DynamoDBOverride[C] = DynamoDBOverride.empty[C]
   val indexNameOpt: Option[String] = None
+
+  def withClient[C1 <: DynamoDBClient](
+    client: C1,
+    overrides: DynamoDBOverride[C1] = DynamoDBOverride.empty[C1]
+  ): TableLike[C1, V, PK] = {
+    val parent = this
+    val c = client
+    val o = overrides
+    new TableLike[C1, V, PK] {
+      override val client: C1 = c
+      override val tableName: String = parent.tableName
+      override val schema: TableSchema.Aux[V, PK] = parent.schema
+      override val overrides: DynamoDBOverride[C1] = o
+    }
+  }
+
+  def withOverrides(overrides: DynamoDBOverride[C]): TableLike[C, V, PK] =
+    withClient(this.client, overrides)
+
+  def noClient: TableLike[DynamoDBClient.Missing, V, PK] =
+    withClient(DynamoDBClient.Missing)
 }
 
 sealed trait TableWithRangeLike[C <: DynamoDBClient, V, PK, RK] extends TableLike[C, V, (PK, RK)] {
   override val schema: TableSchemaWithRange.Aux[V, PK, RK]
+
+  override def withClient[C1 <: DynamoDBClient](
+    client: C1,
+    overrides: DynamoDBOverride[C1] = DynamoDBOverride.empty[C1]
+  ): TableWithRangeLike[C1, V, PK, RK] = {
+    val parent = this
+    val c = client
+    val o = overrides
+    new TableWithRangeLike[C1, V, PK, RK] {
+      override val client: C1 = c
+      override val tableName: String = parent.tableName
+      override val schema: TableSchemaWithRange.Aux[V, PK, RK] = parent.schema
+      override val overrides: DynamoDBOverride[C1] = o
+    }
+  }
+
+  override def withOverrides(overrides: DynamoDBOverride[C]): TableWithRangeLike[C, V, PK, RK] =
+    withClient(this.client, overrides)
+
+  override def noClient: TableWithRangeLike[DynamoDBClient.Missing, V, PK, RK] =
+    withClient(DynamoDBClient.Missing)
 }
 
 sealed class Table[C <: DynamoDBClient, V, PK](
@@ -22,16 +64,16 @@ sealed class Table[C <: DynamoDBClient, V, PK](
   override val schema: TableSchema.Aux[V, PK],
   override val overrides: DynamoDBOverride[C] = DynamoDBOverride.empty[C]
 ) extends TableLike[C, V, PK] {
-  def withClient[C1 <: DynamoDBClient](
+  override def withClient[C1 <: DynamoDBClient](
     client: C1,
     overrides: DynamoDBOverride[C1] = DynamoDBOverride.empty[C1]
   ): Table[C1, V, PK] =
     new Table[C1, V, PK](client, tableName, schema, overrides)
 
-  def withOverrides(overrides: DynamoDBOverride[C]): Table[C, V, PK] =
+  override def withOverrides(overrides: DynamoDBOverride[C]): Table[C, V, PK] =
     new Table[C, V, PK](client, tableName, schema, overrides)
 
-  def noClient: Table[DynamoDBClient.Missing, V, PK] =
+  override def noClient: Table[DynamoDBClient.Missing, V, PK] =
     new Table[DynamoDBClient.Missing, V, PK](DynamoDBClient.Missing, tableName, schema)
 
   def index[IPK](indexSchema: IndexSchema[V, IPK]): Index[C, V, IPK] =
@@ -71,16 +113,16 @@ class Index[C <: DynamoDBClient, V, PK](
 ) extends TableLike[C, V, PK] {
   override val indexNameOpt: Option[String] = Some(indexName)
 
-  def withClient[C1 <: DynamoDBClient](
+  override def withClient[C1 <: DynamoDBClient](
     client: C1,
     overrides: DynamoDBOverride[C1] = DynamoDBOverride.empty[C1]
   ): Index[C1, V, PK] =
     new Index[C1, V, PK](client, tableName, indexName, schema, overrides)
 
-  def withOverrides(overrides: DynamoDBOverride[C]): Index[C, V, PK] =
+  override def withOverrides(overrides: DynamoDBOverride[C]): Index[C, V, PK] =
     new Index[C, V, PK](client, tableName, indexName, schema, overrides)
 
-  def noClient: Index[DynamoDBClient.Missing, V, PK] =
+  override def noClient: Index[DynamoDBClient.Missing, V, PK] =
     new Index[DynamoDBClient.Missing, V, PK](DynamoDBClient.Missing, tableName, indexName, schema)
 }
 
@@ -93,16 +135,16 @@ class IndexWithRange[C <: DynamoDBClient, V, PK, RK](
 ) extends TableWithRangeLike[C, V, PK, RK] {
   override val indexNameOpt: Option[String] = Some(indexName)
 
-  def withClient[C1 <: DynamoDBClient](
+  override def withClient[C1 <: DynamoDBClient](
     client: C1,
     overrides: DynamoDBOverride[C1] = DynamoDBOverride.empty[C1]
   ): IndexWithRange[C1, V, PK, RK] =
     new IndexWithRange[C1, V, PK, RK](client, tableName, indexName, schema, overrides)
 
-  def withOverrides(overrides: DynamoDBOverride[C]): IndexWithRange[C, V, PK, RK] =
+  override def withOverrides(overrides: DynamoDBOverride[C]): IndexWithRange[C, V, PK, RK] =
     new IndexWithRange[C, V, PK, RK](client, tableName, indexName, schema, overrides)
 
-  def noClient: IndexWithRange[DynamoDBClient.Missing, V, PK, RK] =
+  override def noClient: IndexWithRange[DynamoDBClient.Missing, V, PK, RK] =
     new IndexWithRange[DynamoDBClient.Missing, V, PK, RK](DynamoDBClient.Missing, tableName, indexName, schema)
 }
 
